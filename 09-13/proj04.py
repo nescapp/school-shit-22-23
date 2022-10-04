62439a341ed6490f35919811a5b244722fe5e960
nesc4p <97590612+nescapp@users.noreply.github.com>
2022-10-04 11:59:51 +0200

    Update proj04.py

--- 09-13/proj04.py
+++ 09-13/proj04.py
@@ -1,18 +1,18 @@
-essance = 32.5
+essence = 32.5
 distance = int(input("Entrer la distance de votre destination ou entrer 0 pour faire le plain : "))
 consommation = 5 * distance / 100
 
 if distance == 0:
     print("- - - Vous avez fait le plein - - -")
-    print(f"Vous avez {essance} litres d'essance")
+    print(f"Vous avez {essence} litres d'essence")
 else:
     personne = int(input("Entrer le nombre de personnes : "))
     consommation += (consommation * (personne * 0.3))
     distance *= consommation
 
-    if distance > essance:
+    if distance > essence:
         print("- - - Panne d'essence - - -")
-        print(f"Distance parcourue {essance / consommation}")
+        print(f"Distance parcourue {essence / consommation}")
     else:
         print(f"Distance parcourue {distance / consommation}")
 
