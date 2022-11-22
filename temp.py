8c4d38a2dee2de0413bec36b02e04ff884ca3e11
nesc4p <97590612+nescapp@users.noreply.github.com>
2022-11-22 09:32:08 +0100

    temp file

---
+++ temp.py
@@ -0,0 +1,28 @@
+class ToolBox:
+    def __init__(self, tools):
+        self.tools = []
+
+    def add_tool(tool):
+        pass
+    def remove_tool(tool):
+        pass
+
+class Screwdriver:
+    def __init__(self, size):
+        self.size = size
+
+    def tighten(screw):
+        pass
+    def loosen(screw):
+        pass
+
+class Hammer:
+    def __init__(self, color):
+        self.color = color
+
+    def paint():
+        pass
+    def hammer_in(nail):
+        pass
+    def remove(nail):
+        pass
