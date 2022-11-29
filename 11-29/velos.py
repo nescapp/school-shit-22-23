51e08b649aa260a6b256bd4cdeb3bd824a1494c2
nesc4p <97590612+nescapp@users.noreply.github.com>
2022-11-29 11:33:07 +0100

    cool version

--- 11-29/velos.py
+++ 11-29/velos.py
@@ -1,46 +1,33 @@
-""" Consignes :
-- Créer une classe Velos.
-- Créer 2 instances de la classe Vélos: velo1, velo2, 
-
-Dans la classe Velos :
-- Déclarer et initialiser via  le constructeur les 3  attributs suivants  : marque, prix, couleur.
-- Créer une fonction Deplacer() permettant de déplacer velo1 de manière à ce qu'il forme un carré et qu'il puisse revenir à la case de départ.
-- Pour cela importer pour cela le module turtle.
-- Appeler la fonction Pen() permettant le déplacement de l'objet velo1.
-- Pour cela, créer un nouveau canevas(une interface graphique) en appliquant l'instruction suivante :
-          velo1=turtle.Pen()
-
-- Pour déplacer velo1 tu fais appel à des fonctions disponibles dans la variable velo1.
-
- Par exemple, pour dire à velo1 d'avancer de 50 mètres(pixels), tu tapes l'instruction suivante :
-
-velo1.forward(50).
-
-Les autres instructions à vous de les découvrir !
-
-- Afficher les attributs des 2 instances dans le programme principal. """
 import turtle
-
+import time
 class Velos:
     def __init__(self, marque:str, prix:float, couleur:str):
         self.marque = marque
         self.prix = prix
         self.couleur = couleur
 
-def Deplacer(target):
-    """permettant de déplacer velo1 de manière à ce qu'il forme un carré et qu'il puisse revenir à la case de départ"""
-    target.forward(50)
+def Deplacer(target_velo, color):
+    time.sleep(0.5)
+    target_velo.color(color)
+    target_velo.forward(50)
+    target_velo.left(90)
+    target_velo.forward(50)
+    target_velo.left(90)
+    target_velo.forward(50)
+    target_velo.left(90)
+    target_velo.forward(50)
+    target_velo.left(90)
 
 
 velo1 = Velos("a", 1, "bleu")
 velo2 = Velos("b", 2, "rouge")
 
-# create a new canvas
+print(f"vélo n°1 : {velo1.marque, velo1.prix, velo1.couleur}")
+print(f"vélo n°2 : {velo2.marque, velo2.prix, velo2.couleur}")
+
 velo1=turtle.Pen()
+Deplacer(velo1, "blue")
+velo2=turtle.Pen()
+Deplacer(velo2, "red")
 
-# move forward
-Deplacer(velo1)
-# leave the window open until the user clicks on it
 turtle.mainloop()
-
-
