bbe1357819408ddbaf49754f2780ba47263c354e
nesc4p <97590612+nescapp@users.noreply.github.com>
2022-12-02 09:50:42 +0100

    bruh forgor this 💀

--- 12-02/ex3.py
+++ 12-02/ex3.py
@@ -16,9 +16,10 @@ p1 = Points(1, 2)
 p2 = Points(3, 4)
 p3 = Points(5, 6)
 p4 = Points(7, 8)
+p5 = Points(9, 10)
 
 print(p1.get_x(), p1.get_y())
 print(p2.get_x(), p2.get_y())
 print(p3.get_x(), p3.get_y())
 print(p4.get_x(), p4.get_y())
-
+print(p5.get_x(), p5.get_y())
