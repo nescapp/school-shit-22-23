5e1d52d2f76826bb8a3c8d83ba1ba74055e86c6f
nesc4p <97590612+nescapp@users.noreply.github.com>
2023-03-21 11:59:05 +0100

    a

--- 03-21 - latest/test.py
+++ 03-21 - latest/test.py
@@ -9,23 +9,21 @@ class Enfant1(Parent):
     """"""
     
     def bark(self):
-        print(f"\033[3;2m *{self.name} aboie*\033[0m")
+        print("")
 
 
 class Enfant2(Parent):
     """"""
     
     def meow(self):
-        print(f"\033[3;2m *{self.name} miaule*\033[0m")
+        print("a")
 
 
 def main():
     enfant1 = Enfant1("Ryad", ) # create a dog and give it a name
-    print(f"created dog \033[44m {dog.name} \033[0m") # print the dog's name
     dog.bark()
 
     enfant2 = Enfant2("Felix") # create a cat and give it a name
-    print(f"created cat \033[44m {cat.name} \033[0m") # print the cat's name
     cat.meow()
 
 
