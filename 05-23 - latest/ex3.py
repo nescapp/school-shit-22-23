9d3b8b0a4714a06dece7a73a7ffeccdce307197f
nesc4p <97590612+nescapp@users.noreply.github.com>
2023-06-02 09:01:06 +0200

    changed this

--- 05-23 - latest/ex3.py
+++ 05-23 - latest/ex3.py
@@ -1,7 +1,5 @@
-"""
-Crée un programme qui ouvre le fichier animeaux.txt et qui affiche le mot le plus court et le plus long.
-
-"""
+with open('animeaux.txt', 'w') as f:
+    f.write('chien\ncheval\nchat\nsouris\nrat\nlapin\ncochon\nmouton\nvache\npoule\ncoq\ncanard\noie\nchevre\nelephant\nhippopotame\nrhinoceros\nlion\ntigre\nours\nloup\nrenard\nsinge\npanda\nkangourou\ngirafe\nzebre\nantilope\nmarmotte\nchameau\nlama\nalpaga\nchinchilla\nfuret\nherisson\necureuil\nbelette\nblaireau\nraton-laveur\nparesseux\nkoala\nornithorynque\nperroquet\nperruche\ngeai\npie\nmerle\nmoineau\npigeon\nhibou\naigle\nfaucon\nvautour\npoussin\npoulet\ncoquelet')
 
 with open('animeaux.txt', 'r') as f:
     liste = f.readlines()
